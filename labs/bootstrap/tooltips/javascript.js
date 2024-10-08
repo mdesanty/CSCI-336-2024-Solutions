b4266aacafdabd6d84180495303d1786362a2397
Michael DeSanty <michael.desanty@gmail.com>
2024-10-07 20:56:24 -0400

    Lab: Bootstrap Tooltips - Solution

---
+++ labs/bootstrap/tooltips/javascript.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+});
