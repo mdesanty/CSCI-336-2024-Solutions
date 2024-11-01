cb9ba8a84fa02277a508bd5c0e9ab31607da557e
Michael DeSanty <michael.desanty@gmail.com>
2024-11-01 15:36:58 -0400

    Lab: Function Component Props - Solution

---
+++ labs/react/function_component_props/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './components/Greeting';
+
+import './App.css';
+
+function App() {
+  return (
+    <Greeting greeting='Hi' firstName='Mike' lastName='D' />
+  );
+}
+
+export default App;
