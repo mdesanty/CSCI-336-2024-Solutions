b1430908d7b0281d9e1bd8ee7d1a7ae142afcfae
Michael DeSanty <michael.desanty@gmail.com>
2024-11-01 15:36:34 -0400

    Lab: Function Components - Solution

---
+++ labs/react/function_components/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from './components/Greeting';
+
+import './App.css';
+
+function App() {
+  return (
+    <Greeting />
+  );
+}
+
+export default App;
---
+++ labs/react/function_components/src/components/Greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting() {
+  return (
+    <h1>Hello Michael DeSanty</h1>
+  );
+}
+
+export default Greeting;
