3e77c555a9497d91cdc6e20e967954dc8a8b2494
Michael DeSanty <michael.desanty@gmail.com>
2024-12-07 16:08:35 -0500

    Lab: React Bootstrap Getting Started - Solution

---
+++ labs/react_bootstrap/getting_started/src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <Container className="pt-3">
+      <h1>Welcome to React Bootstrap!</h1>
+    </Container>
+  );
+}
+
+export default App;
